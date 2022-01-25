73c9c574765313860a0466b58cfafbec369c588c
Kian Aira <90256501+marquisKian@users.noreply.github.com>
2022-01-25 10:27:56 +0800

    changed variable data type

--- finals_db.sql
+++ finals_db.sql
@@ -1,42 +1,38 @@
 
+CREATE DATABASE covid_db;
 
-CREATE TABLE locations (
+CREATE TABLE locations(
 
-    loc_iso_code TEXT PRIMARY KEY,
-    loc_name VARCHAR(50)
+    loc_name VARCHAR(50) NOT NULL,
+    loc_iso_code VARCHAR(40) PRIMARY KEY
 );
-
-
-
-
-CREATE TABLE  vaccinations_by_manufacturer (
-
-    vbm_id INT PRIMARY KEY,
-    vbm_location VARCHAR(30) NOT NULL,
-    vbm_date DATE NOT NULL,
-    vbm_vaccine VARCHAR(20) NOT NULL,
-    vbm_total_vaccinations  INT NOT NULL
-);
-
-
-
 
 CREATE TABLE vaccines (
 
-    vac_name TEXT PRIMARY KEY;
+    vac_name VARCHAR(50) PRIMARY KEY
+);
+
+CREATE TABLE vaccinations_by_manufacturer (
+
+    vbm_id BIGSERIAL PRIMARY KEY,
+    vbm_location VARCHAR(100) NOT NULL,
+    vbm_date DATE NOT NULL,
+    vbm_vaccine VARCHAR(50) NOT NULL,
+    vbm_total_vaccinations VARCHAR NOT NULL
+
 );
 
 
+CREATE TABLE vaccines_by_location (
 
-CREATE TABLE vaccinations_by_location (
-    vbl_id SERIAL, 
-    vbl_last__observation_date DATE,
-    vbl_source_name VARCHAR(100),
-    vbl_source_website VARCHAR(100)
+    vbl_id BIGSERIAL PRIMARY KEY,
+    vbl_location VARCHAR(100) NOT NULL,
+    vbl_iso_code VARCHAR(20) NOT NULL,
+    vbl_vaccines TEXT NOT NULL,
+    vbl_last__observation_date DATE NOT NULL,
+    vbl_source_name VARCHAR(100) NOT NULL,
+    vbl_source_website VARCHAR(300)
+
 );
-
-
-
-
 
 
